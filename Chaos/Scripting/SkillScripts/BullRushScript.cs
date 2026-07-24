6d81f4fd92264d90a90c9a04bc40061063441974
Ace-Dream10 <rommlozzz25@gmail.com>
2026-07-24 17:46:50 -0500

    Add Bull Rush/Lancer's Charge skill, testing sword, and monster test map
    - Add BullRushScript with path-based rush movement and damage-on-hit,
      and its bull_rush.json template ("Lancer's Charge")
    - Add testing_sword item template
    - Add monsterTest map instance (template 6907, common_rat spawner) and
      fix lightType casing in the 6907 template
    - Fix invalid leading-zero sprite value in common_rat.json
    - Fix coloredItem.json line ending
    - Point starting map/spawn (prod) at monsterTest (33, 19)

---
+++ Chaos/Scripting/SkillScripts/BullRushScript.cs
@@ -0,0 +1,142 @@
+#region
+using Chaos.DarkAges.Definitions;
+using Chaos.Definitions;
+using Chaos.Extensions;
+using Chaos.Extensions.Geometry;
+using Chaos.Geometry.Abstractions;
+using Chaos.Models.Data;
+using Chaos.Models.Panel;
+using Chaos.Models.World.Abstractions;
+using Chaos.Scripting.FunctionalScripts.Abstractions;
+using Chaos.Scripting.FunctionalScripts.ApplyDamage;
+using Chaos.Scripting.SkillScripts.Abstractions;
+#endregion
+
+namespace Chaos.Scripting.SkillScripts;
+
+public class BullRushScript : ConfigurableSkillScriptBase
+{
+    /// <inheritdoc />
+    public BullRushScript(Skill subject)
+        : base(subject)
+        => ApplyDamageScript = ApplyAttackDamageScript.Create();
+
+    /// <inheritdoc />
+    public override void OnUse(ActivationContext context)
+    {
+        var source = context.Source;
+        var map = context.TargetMap;
+
+        var endPoint = source.DirectionalOffset(source.Direction, RushDistance);
+
+        var points = source.GetDirectPath(endPoint)
+                            .Skip(1);
+
+        var lastWalkablePoint = Point.From(source);
+
+        foreach (var point in points)
+        {
+            if (map.IsWall(point) || map.IsBlockingReactor(point))
+            {
+                source.WarpTo(lastWalkablePoint);
+                PlayHitEffects(context, lastWalkablePoint, null);
+
+                return;
+            }
+
+            var creature = map.GetEntitiesAtPoints<Creature>(point)
+                              .TopOrDefault();
+
+            if (creature != null)
+            {
+                if (Filter.IsValidTarget(source, creature))
+                {
+                    source.WarpTo(lastWalkablePoint);
+
+                    var damage = CalculateDamage(source);
+
+                    if (damage > 0)
+                        ApplyDamageScript.ApplyDamage(source, creature, this, damage);
+
+                    PlayHitEffects(context, creature);
+                } else
+                {
+                    source.WarpTo(lastWalkablePoint);
+                    PlayHitEffects(context, lastWalkablePoint, creature);
+                }
+
+                return;
+            }
+
+            lastWalkablePoint = point;
+        }
+
+        source.WarpTo(lastWalkablePoint);
+    }
+
+    private int CalculateDamage(Creature source)
+    {
+        var damage = BaseDamage ?? 0;
+
+        if (!DamageStat.HasValue)
+            return damage;
+
+        var statValue = source.StatSheet.GetEffectiveStat(DamageStat.Value);
+
+        damage += DamageStatMultiplier.HasValue ? Convert.ToInt32(statValue * DamageStatMultiplier.Value) : statValue;
+
+        return damage;
+    }
+
+    private void PlayHitEffects(ActivationContext context, Creature creature)
+    {
+        if (Sound.HasValue)
+            context.TargetMap.PlaySound(Sound.Value, Point.From(creature));
+
+        if (Animation != null)
+            creature.Animate(Animation, context.Source.Id);
+    }
+
+    private void PlayHitEffects(ActivationContext context, IPoint point, Creature? creature)
+    {
+        if (Sound.HasValue)
+            context.TargetMap.PlaySound(Sound.Value, point);
+
+        if (Animation != null)
+        {
+            if (creature != null)
+                creature.Animate(Animation, context.Source.Id);
+            else
+                context.TargetMap.ShowAnimation(Animation.GetPointAnimation(point, context.Source.Id));
+        }
+    }
+
+    #region ScriptVars
+    public IApplyDamageScript ApplyDamageScript { get; init; }
+
+    /// <inheritdoc cref="Chaos.Scripting.Components.AbilityComponents.DamageAbilityComponent.IDamageComponentOptions.BaseDamage" />
+    public int? BaseDamage { get; init; }
+
+    /// <inheritdoc cref="Chaos.Scripting.Components.AbilityComponents.DamageAbilityComponent.IDamageComponentOptions.DamageStat" />
+    public Stat? DamageStat { get; init; }
+
+    /// <inheritdoc cref="Chaos.Scripting.Components.AbilityComponents.DamageAbilityComponent.IDamageComponentOptions.DamageStatMultiplier" />
+    public decimal? DamageStatMultiplier { get; init; }
+
+    /// <summary>
+    ///     The filter used to determine whether the first creature encountered is a valid (hostile) target
+    /// </summary>
+    public TargetFilter Filter { get; init; }
+
+    /// <summary>
+    ///     The maximum number of tiles the caster will rush forward
+    /// </summary>
+    public int RushDistance { get; init; } = 5;
+
+    /// <inheritdoc cref="Chaos.Scripting.Components.AbilityComponents.SoundAbilityComponent.ISoundComponentOptions.Sound" />
+    public byte? Sound { get; init; }
+
+    /// <inheritdoc cref="Chaos.Scripting.Components.AbilityComponents.AnimationAbilityComponent.IAnimationComponentOptions.Animation" />
+    public Animation? Animation { get; init; }
+    #endregion
+}
